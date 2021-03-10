6803cdcd1e77add7bd8e29c7d94f87bcd9614e12
AndrewJBateman <gomezbateman@yahoo.com>
2021-03-10 15:55:14 +0100

    dependencies updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-material-blog';
   results;
   constructor(http: HttpClient) {
-    this.results = http.get("http://localhost:5001/angular-blog-14cef/us-central1/api/database_ref")
+    this.results = http.get(
+      'http://localhost:5001/angular-blog-14cef/us-central1/api/database_ref'
+    );
   }
 }
